53c3ba95f69f40ba13a00da15c800c7cf12ae38f
devtripletech <145587920+devtripletech@users.noreply.github.com>
2024-07-01 15:35:33 -0300

    script junto

---
+++ public_html/js/script.js
@@ -0,0 +1,39 @@
+  
+function mascaraDecimal(obj, event) {	  
+	(function($) {		
+		if(event.keyCode == 8 || event.keyCode == 46){
+			return true;
+		}
+		$(obj).maskMoney({prefix:'R$ ', allowNegative: true, thousands:'.', decimal:',', affixesStay: false});	
+	}(jQuery));
+};
+
+function somenteNumero(obj, event) {	  
+	(function($) {		
+		if(event.keyCode == 8 || event.keyCode == 46){
+			return true;
+		}		
+		$(obj).numeric();
+	}(jQuery));
+};
+
+function removerMascara(id) {	  
+	(function($) {
+		var value = document.getElementById(id).value;
+		if(value.indexOf('R$')){
+			value = value.replace('R$', '');
+		}
+	    while(value.indexOf('.') != -1 && value.indexOf(',') != -1 ){
+			value = value.replace('.', '');
+		}
+		if(value.indexOf(',') != -1){
+			value = value.replace(',', '.');		
+		}
+		document.getElementById(id).value = Number(value);		
+	}(jQuery));
+};
+
+ $('.selectpicker').selectpicker({
+          style: 'btn-info',
+          size: 4
+      });
